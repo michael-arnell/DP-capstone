14c30e497f03e53aa1bf7a4f4fe2a7db1dc66e04
Michael Arnell <michael.olson.arnell@gmail.com>
2021-12-04 12:25:25 -0700

    Add manager methods

--- main.py
+++ main.py
@@ -20,3 +20,41 @@ if current_user.user_type == 'manager':
     current_user = Manager(current_user.user_id, current_user.first_name, current_user.last_name, current_user.email, current_user.city, current_user.state, current_user.postal_code, current_user.phone, current_user.user_type)
 
 print('\nWelcome ' + current_user.first_name + ' ' + current_user.last_name + '. You are now logged in.')
+
+menu_choice = None
+if current_user.user_type == 'user':
+    pass
+elif (current_user.user_type) == 'manager':
+    while menu_choice != 'E':
+        menu_choice = input('''\nPlease select an action to take:
+
+(A)dd a record
+(M)odify a record
+(V)iew a report
+(E)xit application\n''').upper()
+        if menu_choice == 'V':
+            report_choice = None
+            while report_choice != 'B':
+                report_choice = input('''\nPlease select the record type you would like to add:
+
+(A)ll users
+(C)ompetency
+(A)ssessment
+(R)esult of assessment
+(B)ack to Main Menu\n''').upper()
+                if report_choice == 'A':
+                    current_user.view_users(cursor)
+                elif report_choice == 'S':
+                    current_user.view_user(cursor)
+        if menu_choice == 'A':
+            record_choice = None
+            while record_choice != 'B':
+                record_choice = input('''\nPlease select the record type you would like to add:
+
+(U)ser
+(C)ompetency
+(A)ssessment
+(R)esult of assessment
+(B)ack to Main Menu\n''').upper()
+                if record_choice == 'U':
+                    current_user.add_user(cursor, connection)
--- manager.py
+++ manager.py
@@ -1,12 +1,73 @@
 from user import User
+import bcrypt
 
 class Manager(User):
     def __init__(self, user_id, first_name, last_name, email, city, state, postal_code, phone, user_type):
         super().__init__()
         super().get_user(user_id, first_name, last_name, email, city, state, postal_code, phone, user_type)
 
-    def add_user(self):
+    def view_users(self, cursor):
+        query = 'SELECT user_id, first_name, last_name, email, city, state, postal_code, phone, user_type from Users;'
+        rows = cursor.execute(query).fetchall()
+        print('''User ID   First Name  Last Name       Email                City           State  Postal Code  Phone      User Type''')
+        for idx in range(len(rows)):
+            this_user = list(rows[idx])
+            for field in range(len(this_user)):
+                if not this_user[field]:
+                    this_user[field] = ' '
+            user_id, first_name, last_name, email, city, state, postal_code, phone, user_type = this_user
+            print(f'{user_id:^10}{first_name:<12}{last_name:<16}{email:<20}{city:<19}{state:<5}{postal_code:<8}{phone:<15}{user_type:<15}')
+
+    def view_user(self, cursor):
+        email = input('Please enter the email of the user you would like to view: ')
+        values = (email,)
+        query = 'SELECT user_id, first_name, last_name, email, city, state, postal_code, phone, user_type from Users where email = ?;'
+        user = cursor.execute(query, values).fetchone()
+        user_id, first_name, last_name, email, city, state, postal_code, phone, user_type = user
+        print(f'''\nUser ID: {user_id}
+First Name:  {first_name}
+Last Name:   {last_name}
+Email:       {email}
+City:        {city}
+State:       {state}
+Postal Code: {postal_code}
+Phone:       {phone}
+User Type:   {user_type}
+        ''')
+
+    def view_competencies_by_user(self, cursor):
         pass
+
+    def view_single_competency(self, cursor):
+        pass
+
+    def view_single_user_competencies(self, cursor):
+        pass
+
+    def view_single_user_assessments(self, cursor):
+        pass
+
+    def add_user(self, cursor, connection):
+        query = 'INSERT into Users (first_name, last_name, email, password, city, state, postal_code, phone, user_type) \
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);'
+        first_name = input('\nFirst Name: ')
+        last_name = input('Last Name: ')
+        email = input('Email: ')
+        password = bcrypt.hashpw(input('Password: ').encode('utf-8'),self.salt)
+        city = input('City: ')
+        state = input('State: ')
+        postal_code = input('Postal Code: ')
+        phone = input('Phone: ')
+        user_choice = None
+        while user_choice not in ['U','M']:
+            user_choice = input('User Type - Please select (U)ser or (M)anager: ').upper()
+        if user_choice == 'U':
+            user_type = 'user'
+        else:
+            user_type = 'manager'
+        values = (first_name, last_name, email, password, city, state, postal_code, phone, user_type)
+        cursor.execute(query,values)
+        connection.commit()
 
     def add_competency(self):
         pass
